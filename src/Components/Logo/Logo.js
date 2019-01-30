46f7a32413db99e3e7cff7e7da7afabc6c84c623
Neil Ghulam <neal.ghulam@gmail.com>
2019-01-30 20:28:35 +0200

    added testing info

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -7,7 +7,7 @@ import brains from './brains.png';
 const Logo = () => {
     return(
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150,}} >
                 <div className="Tilt-inner pa3"><img style={{paddingTop: '30px'}} src={brains} alt="logo" ></img> </div>
             </Tilt>
         </div>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -7,6 +7,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
                 <nav style={{display: 'flex', justifyContent: 'space-between',}}>
                     <div className="">
                         <p className="f3 pa3 b" style={{zIndex: 1}} >Neil Ghulam</p>
+                        
                     </div>
                     <div style={{display: 'flex', justifyContent: '',}}>
                        <p onClick={() => onRouteChange('signout')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Sign Out</p>
@@ -20,6 +21,14 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
             <nav style={{display: 'flex', justifyContent: 'space-between',}}>
                 <div className="">
                     <p className="f3 pa3 b" style={{zIndex: 1}} >Neil Ghulam</p>
+                    <p className="f5 pa3 dark-blue b" style={{zIndex: 1}} >If you dont want to register 
+                    <p>please use this to login and test.</p>
+                    </p>
+                    <p className="f6 pa3 dark-blue" style={{zIndex: 1}}>
+                    Email: test@test.com 
+                    <p>Password: test</p>
+                    </p>
+                   
                 </div>
                 <div style={{display: 'flex', justifyContent: '',}}>
                     <p onClick={() => onRouteChange('signIn')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Sign In</p>

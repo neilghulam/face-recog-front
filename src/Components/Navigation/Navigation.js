ca46060a778e7c7c2c41d6ab1be187f141cb4e0c
Neil Ghulam <neal.ghulam@gmail.com>
2019-01-30 20:10:19 +0200

    added name to project

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
+
         if(isSignedIn) {
             return(
-                <nav style={{display: 'flex', justifyContent: 'flex-end',}}>
-                 <p onClick={() => onRouteChange('signout')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Sign Out</p>
-             </nav>
+                <nav style={{display: 'flex', justifyContent: 'space-between',}}>
+                    <div className="">
+                        <p className="f3 pa3 b" style={{zIndex: 1}} >Neil Ghulam</p>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: '',}}>
+                       <p onClick={() => onRouteChange('signout')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Sign Out</p>
+                    </div>  
+                </nav>
             )
             
         } else {
             return(
-            <nav style={{display: 'flex', justifyContent: 'flex-end',}}>
-                <p onClick={() => onRouteChange('signIn')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Sign In</p>
-                <p onClick={() => onRouteChange('register')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Register</p>
-             </nav>
+            
+            <nav style={{display: 'flex', justifyContent: 'space-between',}}>
+                <div className="">
+                    <p className="f3 pa3 b" style={{zIndex: 1}} >Neil Ghulam</p>
+                </div>
+                <div style={{display: 'flex', justifyContent: '',}}>
+                    <p onClick={() => onRouteChange('signIn')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Sign In</p>
+                    <p onClick={() => onRouteChange('register')} className="f3 link dim black underline pa3 pointer" style={{zIndex: 1}} >Register</p>
+             
+                </div>
+                </nav>
             )
             
         }
